e95d2059be3dfdf25c79b2e12b3b285e84804388
Caíque Andrade <cahwayan@gmail.com>
2016-10-04 00:38:56 -0300

    Teste

--- app/src/main/java/com/tcc/dagon/opus/CadastroActivity.java
+++ app/src/main/java/com/tcc/dagon/opus/CadastroActivity.java
@@ -42,7 +42,7 @@ public class CadastroActivity extends AppCompatActivity {
     private StringRequest request;
     private ProgressDialog progresso;
 
-
+    // metodo construtor
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
