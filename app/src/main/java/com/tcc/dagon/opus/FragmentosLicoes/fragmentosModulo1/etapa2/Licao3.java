9ba46a74fa9e448a1ead00b0a120e949ab4b68fd
Caíque Andrade <cahwayan@gmail.com>
2016-10-26 17:59:15 -0200

    Avançado progresso

--- app/src/main/java/com/tcc/dagon/opus/FragmentosLicoes/fragmentosModulo1/etapa2/Licao3.java
+++ app/src/main/java/com/tcc/dagon/opus/FragmentosLicoes/fragmentosModulo1/etapa2/Licao3.java
@@ -1,21 +1,39 @@
 package com.tcc.dagon.opus.FragmentosLicoes.fragmentosModulo1.etapa2;
-
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import com.tcc.dagon.opus.ContainerLicoes.Modulos.Modulo1.ContainerModulo1Etapa1;
+import com.tcc.dagon.opus.ContainerLicoes.Modulos.Modulo1.ContainerModulo1Etapa2;
 import com.tcc.dagon.opus.R;
 
 /**
  * Created by charlinho on 09/10/2016.
  */
 public class Licao3 extends Fragment {
+    Button btnAvancar;
+    ViewPager mViewPager;
+    View viewRoot;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_modulo1_etapa1_licao3, container, false);
+        viewRoot = inflater.inflate(R.layout.fragment_modulo1_etapa2_licao3,container,false);
+        btnAvancar = (Button) viewRoot.findViewById(R.id.btnAvancar);
+        mViewPager = ((ContainerModulo1Etapa2)getActivity()).getPager();
+        listeners();
+        return viewRoot;
+    }
+
+    private void listeners() {
+        btnAvancar.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(3);
+            }
+        });
     }
 }
